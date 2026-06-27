2b6ef4cde416c1e5dd26dc470d644c8bff3788e0
Hatim Rampurawala <hatimrampurawala6@gmail.com>
2026-06-27 16:48:49 +0530

    arrays part one

---
+++ tutorial/02-basics/01_array.js
@@ -0,0 +1,22 @@
+// array
+
+const myArr = [0, 1, 2, 3, 4, 5]
+// console.log(myArr[0]);
+
+const myArr2 = new Array(1, 2, 3, 4)
+// console.log(myArr2[1]);
+
+// Array methods
+
+// myArr.push(6)
+// myArr.push(7)
+// myArr.pop()
+// console.log(myArr);
+
+// slice and splice
+
+console.log("A ", myArr);
+const myn1 = myArr.slice(1, 3)
+
+console.log(myn1);
+console.log("B ", myArr);
